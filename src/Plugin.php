d0c974ce3f591ba6aa45a3141271f48269e16ad9
Proggery <proggery3333@gmail.com>
2025-05-28 13:17:03 +0200

    fájlok átírva

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,5 +1,5 @@
 <?php
-namespace VwStripeSubscription;
+namespace VwPluginStripeSubscription;
 
 use Dotenv\Dotenv;
 
--- vw-stripe-subscription.php
+++ vw-stripe-subscription.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: VW Stripe Subscription
+ * Plugin Name: VW Plugin Stripe Subscription
  * Description: Stripe előfizetés kezelés WordPress-ben (Basic & Pro)
  * Version:     1.0.0
  * Author:      Your Name
@@ -12,16 +12,16 @@ require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 require_once plugin_dir_path( __FILE__ ) . 'src/Plugin.php';
 
 // Aktiváláskor beállítjuk a plugin szükséges dolgait
-register_activation_hook( __FILE__, 'vw_stripe_subscription_activate' );
+register_activation_hook( __FILE__, 'vw_plugin_stripe_subscription_activate' );
 function vw_stripe_subscription_activate() {
     // Itt helyezheted el a telepítési műveleteket, ha szükséges
 }
 
 // Deaktiváláskor törölhetjük a beállításokat
-register_deactivation_hook( __FILE__, 'vw_stripe_subscription_deactivate' );
-function vw_stripe_subscription_deactivate() {
+register_deactivation_hook( __FILE__, 'vw_plugin_stripe_subscription_deactivate' );
+function vw_plugin_stripe_subscription_deactivate() {
     // Itt végezheted el a plugin deaktiválási műveleteit
 }
 
 // Beállítjuk a plugin működését
-add_action( 'init', ['\VwStripeSubscription\Plugin', 'init'] );
+add_action( 'init', ['\VwPluginStripeSubscription\Plugin', 'init'] );
